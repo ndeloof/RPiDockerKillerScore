a56a321662f74985fddb01295590262eed53ee21
Nicolas De loof <nicolas.deloof@gmail.com>
2015-10-07 14:30:25 +0200

    Yoann recommend large swap blocks

--- DockerPi/swap.sh
+++ DockerPi/swap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dd if=/dev/zero of=/swapfile bs=1024 count=65536
+dd if=/dev/zero of=/swapfile bs=1M count=64
 mkswap /swapfile
 swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
