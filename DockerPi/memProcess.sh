dce4bdb0fd2b2dccf45a4a1903278c1e81108b62
lhuet <laurent.huet@gmail.com>
2015-10-13 21:56:59 +0200

    conf lxc + scripts memProcess + script kill webservers

---
+++ DockerPi/memProcess.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" = "" ] ; then
+  echo -n "Nom du process : "
+  read process
+else
+  process=$1
+fi
+
+ps aux | grep $process | grep -v grep | awk 'BEGIN { sum=0 } {sum=sum+$6; } END {printf("Taille RAM utilisée: %s Mo\n",sum / 1024)}'
